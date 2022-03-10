3c3a46f95737e91f54cbdf4ca0b6e4ea4302ea4f
ComputerLab <roccoz2004@gmail.com>
2022-03-10 08:58:38 -0800

    Complete Text Reversal

--- src/dev/rzuniga/textreversal/Main.java
+++ src/dev/rzuniga/textreversal/Main.java
@@ -1,8 +1,14 @@
 package dev.rzuniga.textreversal;
+import java.lang.*;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+	    System.out.println(reverseString("STRING INPUT"));
+    }
+    public static String reverseString(String str) {
+        StringBuilder output = new StringBuilder();
+        output.append(str);
+        output.reverse();
+        return String.valueOf(output);
     }
 }
