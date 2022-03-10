50600e880e189d09be69dcf3c4fb5cbd207fa3b7
ComputerLab <roccoz2004@gmail.com>
2022-03-10 09:14:39 -0800

    Complete Palindrome

--- src/dev/rzuniga/palindrome/Main.java
+++ src/dev/rzuniga/palindrome/Main.java
@@ -1,8 +1,21 @@
 package dev.rzuniga.palindrome;
+import java.lang.*;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        queryPalindrome("mdfam");
+    }
+    public static boolean queryPalindrome(String str) {
+        boolean isPalindrome = false;
+        StringBuilder output = new StringBuilder();
+        output.append(str);
+        output.reverse();
+        if (String.valueOf(output).equals(str)) {
+            isPalindrome = true;
+            System.out.println(isPalindrome);
+        } else {
+            System.out.println(isPalindrome);
+        }
+        return isPalindrome;
     }
 }
